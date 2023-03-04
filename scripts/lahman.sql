1d1fa882fccafeddc63c61e31da31ddcb8e68559
Patrick Shanks <patrick.shanks42@gmail.com>
2023-03-04 10:52:57 -0600

    finished 8

--- scripts/lahman.sql
+++ scripts/lahman.sql
@@ -224,8 +224,28 @@ WHERE max_wins = wins AND wswin = 'Y'
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
+SELECT distinct 
+		t.name as team_name,
+		park_name,
+		h.attendance/h.games as avg_attendance
+FROM homegames as h
+INNER JOIN teams as t
+	ON h.team = t.teamid
+	AND h.attendance = t.attendance
+INNER JOIN parks as p
+	ON h.park = p.park
+WHERE h.year = 2016
+	AND h.games>10
+-- ORDER BY avg_attendance DESC
+ORDER BY avg_attendance ASC
+LIMIT 5
+	
+
+
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+SELECT *
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
